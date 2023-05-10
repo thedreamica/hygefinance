d9cfd01df779322c6da831c5d19a5493ebceadad
resparx <ramprash.rspx@gmail.com>
2023-05-11 00:02:24 +0530

    fix: hero styles

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,30 +4,37 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material';
 import Image from 'next/image';
+import { CheckCircleFill } from '@styled-icons/bootstrap/CheckCircleFill'
 
 const Hero = () => {
     const { spacing } = useTheme();
     return <Box sx={{
         backgroundColor: '#EEB609',
         width: '100vw',
-        height: '100vh',
         display: 'flex',
         flexDirection: { xs: 'column-reverse', md: 'row' },
-        padding: { xs: '', md: '80px 140px' }
+        padding: { xs: '', md: '140px 0 80px 140px' }
     }}>
         <Box sx={{
             width: { xs: '100%', md: '50%' },
         }}>
             <Typography variant="h2" sx={{
                 color: '#000000',
-                fontWeight: 700
+                fontWeight: 700,
+                width: '70%',
+                fontSize: '80px',
+                marginBottom: spacing(8),
+                lineHeight: spacing(15)
             }}>
                 Get Instant
                 Loans with Low Interest
             </Typography>
-            <Typography variant="h6" sx={{
+            <Typography variant="subtitle1" sx={{
                 color: '#000000',
-                fontWeight: 400
+                fontWeight: 400,
+                width: '40%',
+                lineHeight: spacing(5),
+                marginBottom: spacing(6),
             }}>
                 Our team of experts uses a methodology to
                 Identify your loan needs.
@@ -38,12 +45,17 @@ const Hero = () => {
                     xs: 'column',
                     md: 'row'
                 },
-                gap: spacing(1.5)
+                gap: spacing(1.5),
+                marginBottom: spacing(3),
             }}>
                 <TextField
                     id="outlined-email"
                     placeholder="Enter email address"
                     type="email"
+                    sx={{
+                        borderRadius: '10px',
+                        backgroundColor: '#FFFFFF'
+                    }}
                     InputLabelProps={{
                         shrink: true,
                     }}
@@ -51,16 +63,27 @@ const Hero = () => {
                 <Button variant="contained">Get started</Button>
             </Box>
             <Box sx={{
-                display: 'flex'
+                display: 'flex',
+                gap: spacing(4)
             }}>
-                <Box>
+                <Box sx={{
+                    display: 'flex',
+                    gap: spacing(2),
+                    alignItems: 'center'
+                }}>
+                    <CheckCircleFill size={16} fill="#000000" />
                         <Typography sx={{
                             color: '#000000',
                         }}>
                         Get 30 day free trial
                         </Typography>
                 </Box>
-                <Box>
+                <Box  sx={{
+                    display: 'flex',
+                    gap: spacing(2),
+                    alignItems: 'center'
+                }}>
+                <CheckCircleFill size={16} fill="#000000" />
                 <Typography sx={{
                     color: '#000000',
                 }}>
@@ -72,8 +95,8 @@ const Hero = () => {
         <Box>
             <Image
                 src="BQwkKh9.png"
-                width={500}
-                height={500}
+                width={740}
+                height={640}
                 alt="Picture of the author"
             />
         </Box>
---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Poppins',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
+  });
+  
